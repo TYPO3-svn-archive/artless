b3e5189ac077509bb0161743a7fa4958a110ea40
direi <direi@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-06-08 18:33:54 +0000

    Added TSconfig files
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/artless/trunk@34027 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ tsconfig/TCEFORM.ts
@@ -0,0 +1,30 @@
+TCEFORM {
+	tt_content {
+		header_layout {
+			removeItems = 100
+			altLabels {
+				0 = Normal
+				1 = <h1>
+				2 = <h2>
+				3 = <h3>
+				4 = <h4>
+				5 = <h5>
+			}
+			addItems {
+				6 = <h6>
+			}
+		}
+	}
+}
+
+#TCEFORM.pages.layout {
+	#disabled = 0
+	#altLabels {
+		#0 = 24
+		#1 = 16 8
+		#2 = Layout 1-1-1
+		#3 = Layout 1-2
+	}
+	#removeItems = 2, 3
+	#addItems.4 = new item
+#}
---
+++ tsconfig/TSconfig.ts
@@ -0,0 +1,7 @@
+### Add this line to the TSconfig of your root page:
+### <INCLUDE_TYPOSCRIPT: source="FILE: EXT:artless/tsconfig/TSconfig.ts">
+### Adjust values afterwards!
+
+<INCLUDE_TYPOSCRIPT: source="FILE: EXT:artless/tsconfig/mod.ts">
+<INCLUDE_TYPOSCRIPT: source="FILE: EXT:artless/tsconfig/RTE.ts">
+<INCLUDE_TYPOSCRIPT: source="FILE: EXT:artless/tsconfig/TCEFORM.ts">
