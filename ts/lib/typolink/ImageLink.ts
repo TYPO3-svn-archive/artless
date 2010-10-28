e7d8dc4b35a4aced2459f326e6a5a76dd40a102b
direi <direi@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-10-28 18:11:44 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/artless/trunk@39636 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ts/lib/typolink/ImageLink.ts
@@ -0,0 +1,17 @@
+A.lib.typolink.ImageLink {
+
+	typolink {
+	
+	parameter.cObject = IMG_RESOURCE
+	parameter.cObject {
+	
+		file {
+			import = uploads/media/
+			import.current = 1
+		}
+		
+	}
+	
+	}
+	
+}
