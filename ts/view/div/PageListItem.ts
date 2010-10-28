fd1ea0cc36287c19f2cac87405c012d96fba7d14
direi <direi@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-10-28 18:12:40 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/artless/trunk@39638 735d13b6-9817-0410-8766-e36946ffe9aa

--- ts/view/div/PageListItem.ts
+++ ts/view/div/PageListItem.ts
@@ -3,6 +3,10 @@ A.view.div.PageListItem {
 	
 	wrap = <div class="A_pagelistitem">|</div>
 	
-	100 =< A.model.Page.h3.titleLinked
+	100 =< A.model.Page.title
+	100 {
+		wrap = <h3>|</h3>
+		typolink < A.lib.typolink.PageLink
+	}
 	
 }
