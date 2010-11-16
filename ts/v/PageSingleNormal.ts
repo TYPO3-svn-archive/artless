8cccd45cc12f3cd88a10f096ed6c78891cf74061
direi <direi@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-11-16 04:56:29 +0000

    Added the views A.v.Content and A.v.Comments
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/artless/trunk@40242 735d13b6-9817-0410-8766-e36946ffe9aa

--- ts/v/PageSingleNormal.ts
+++ ts/v/PageSingleNormal.ts
@@ -2,20 +2,9 @@ A.v.PageSingleNormal = COA
 A.v.PageSingleNormal {
 	
 	wrap = <div class="A_pagesinglenormal">|</div>
-
-	# "Page header"	
+	
 	100 =< A.m.Page.header
-	
-	# "Page content"
-	200 =< A.m.Page.content
-	
-	# "Additional elements"
-	300 = COA
-	300 {
-		wrap = <div class="A_comments">|</div>
-		stdWrap.fieldRequired = tx_artless_enablecomments
-		100 =< A.v.CommentList
-		200 =< A.v.CommentForm
-	}
+	200 =< A.v.Content
+	300 =< A.v.Comments
 	
 }
