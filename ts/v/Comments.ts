73d2e24e588a55f9428ed3915548886bf1e640b7
direi <direi@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-11-16 02:40:14 +0000

    Added a view for Comments that combines A.v.CommentList and A.v.CommentForm, it is only displayed when the field tx_artless_enablecomments is enabled
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/artless/trunk@40233 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ts/v/Comments.ts
@@ -0,0 +1,11 @@
+A.v.Comments = COA
+A.v.Comments {
+	
+	wrap = <div class="A_comments">|</div>
+	
+	100 =< A.v.CommentList
+	200 =< A.v.CommentForm
+	
+	stdWrap.fieldRequired = tx_artless_enablecomments
+	
+}
