bb9a679eb55af9cc0b06e1dd932104356bad6e19
direi <direi@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-11-16 04:46:59 +0000

    Moved page.ts, config.ts and plugin.ts to artess/ts/
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/artless/trunk@40237 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ts/page.ts
@@ -0,0 +1,15 @@
+page < A.c.normal
+
+[globalVar = TSFE:page|layout = 1]
+
+page < A.c.layout1
+
+[globalVar = TSFE:page|layout = 2]
+
+page < A.c.layout2
+
+[globalVar = TSFE:page|layout = 3]
+
+page < A.c.layout3
+
+[global]
