e7b332bab40a46ed2860cd7f6ee871b3518a41e0
direi <direi@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-11-16 05:14:55 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/artless/trunk@40245 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ts/c/Layout2.ts
@@ -0,0 +1,8 @@
+A.c.layout2 < A.c.normal
+A.c.layout2 {
+	
+	wrap = <div class="A_container A_layout2">|</div>
+	
+	300 =< A.v.PageSingleLayout2
+	
+}
---
+++ ts/c/Layout3.ts
@@ -0,0 +1,8 @@
+A.c.layout3 < A.c.normal
+A.c.layout3 {
+	
+	wrap = <div class="A_container A_layout3">|</div>
+	
+	300 =< A.v.PageSingleLayout3
+	
+}
