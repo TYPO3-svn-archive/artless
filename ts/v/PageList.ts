b434ea1cd3ac946c20e501064de426e6eac9e6a2
direi <direi@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-11-24 14:40:51 +0000

    Changed HMENU to CONTENT
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/artless/trunk@40555 735d13b6-9817-0410-8766-e36946ffe9aa

--- ts/v/PageList.ts
+++ ts/v/PageList.ts
@@ -1,10 +1,16 @@
 A.v.PageList = CONTENT
 A.v.PageList {
+	
 	wrap = <div class="A_pagelist">|</div>
+	
 	table = pages
+	
 	select {
 		pidInList = {$A.pids.pageList}
-		orderBy = tx_artless_startdate desc
+		orderBy = sorting
+		max = {$A.maxItems.pageList}
 	}
+	
 	renderObj =< A.v.PageListItem
-}	
+	
+}
--- ts/v/PageListBrowsable.ts
+++ ts/v/PageListBrowsable.ts
@@ -4,15 +4,14 @@ A.v.PageListBrowsable {
 	wrap = <div class="A_pagelistbrowsable A_pagelist">|</div>
 	
 	select {
-		pidInList = {$A.pids.pageList}
-		max = {$A.maxItems.pageListBrowsable}
+		pidInList = {$A.pids.pageListBrowsable}
 		begin.cObject = TEXT
 		begin.cObject.value = {$A.maxItems.pageListBrowsable}
 		begin.cObject {
 			stdWrap.dataWrap = (| * {GPvar:tx_pagebrowse_pi1|page}) + 1
 			prioriCalc = intval
 		}
-		
+		max = {$A.maxItems.pageListBrowsable}
 	}
 	
 	stdWrap.append =< A.h.PageBrowser
