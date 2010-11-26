803070f522fef65d728c84d4674a4e91f139b39c
direi <direi@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-11-26 06:00:04 +0000

    Added view for browser title: <title>{page:title} - {sitetitle}</title>
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/artless/trunk@40584 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ts/v/BrowserTitle.ts
@@ -0,0 +1,17 @@
+A.v.BrowserTitle = COA
+A.v.BrowserTitle {
+	
+	wrap = <title>|</title>
+	
+	100 = TEXT
+	100 {
+		field = title
+		noTrimWrap = || - |
+	}
+	
+	200 = TEXT
+	200 {
+		value < sitetitle
+	}
+	
+}
