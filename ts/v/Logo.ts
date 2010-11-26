b4ae89960d10c03a734cd5b8607cc07b12f91326
direi <direi@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-11-26 06:04:10 +0000

    Added views for the title and the logo of the "site" These values can be set in the template record Both link to the home (A.h.linkToHome)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/artless/trunk@40586 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ts/v/Logo.ts
@@ -0,0 +1,17 @@
+A.v.Logo = IMAGE
+A.v.Logo {
+	
+	stdWrap.wrap = <div class="A_logo">|</div>
+	
+	file {
+		import.cObject = TEXT
+		import.cObject {
+			value < resources
+			wrap = uploads/tf/|
+			listNum = 0
+		}
+	}
+	
+	stdWrap.typolink < A.h.linkToHome
+	
+}
---
+++ ts/v/Title.ts
@@ -0,0 +1,10 @@
+A.v.Title = TEXT
+A.v.Title {
+	
+	wrap = <h1 class="A_title">|</h1>
+	
+	value < sitetitle
+	
+	typolink < A.h.linkToHome
+	
+}
