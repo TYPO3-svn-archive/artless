8f8fef0d038dd8347b865fe739e5e1e79bf9ca4e
direi <direi@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-11-26 05:33:21 +0000

    Added stdWrap for CONTENT.select.begin
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/artless/trunk@40577 735d13b6-9817-0410-8766-e36946ffe9aa

--- misc/class.ux_tslib_content.php
+++ misc/class.ux_tslib_content.php
@@ -3,7 +3,8 @@
 class ux_tslib_cObj extends tslib_cObj {
 	
 	function getQuery($table, $conf, $returnQueryArray = FALSE) {
-		$conf['selectFields'] =	$this->stdWrap($conf['selectFields'], $conf['selectFields.']);
+		$conf['selectFields'] = $this->stdWrap($conf['selectFields'], $conf['selectFields.']);
+		$conf['begin'] = $this->stdWrap($conf['begin'], $conf['begin.']);
 		return parent::getQuery($table, $conf, $returnQueryArray);
 	}
 	
