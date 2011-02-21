f76d8b42604a72f811b6008f2305c794a746938c
direi <direi@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-02-21 17:21:53 +0000

    TCA Cleanup Renamed tx_artless_relatedpages to tx_artless_relatives Merged tx_artless_authorpages and tx_artless_authorfeusers to tx_artless_owners Added tx_artless_participants
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/artless/trunk@43973 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -4,14 +4,15 @@ if (!defined ('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
+# var_dump($TCA['pages']['columns']);
+
 $tempColumns = array(
 	'crdate' => array(
 		'exclude' => 1,
 		'label' => 'LLL:EXT:artless/locallang_db.xml:pages.crdate',
-		'label' => 'Creation date:',
 		'config' => array (
 			'type'     => 'input',
-			'size'     => '12',
+			'size'     => '13',
 			'max'      => '20',
 			'eval'     => 'datetime',
 			'checkbox' => '0',
@@ -23,7 +24,7 @@ $tempColumns = array(
 		'label' => 'LLL:EXT:artless/locallang_db.xml:pages.tx_artless_startdate',
 		'config' => array (
 			'type'     => 'input',
-			'size'     => '12',
+			'size'     => '13',
 			'max'      => '20',
 			'eval'     => 'datetime',
 			'checkbox' => '0',
@@ -35,45 +36,26 @@ $tempColumns = array(
 		'label' => 'LLL:EXT:artless/locallang_db.xml:pages.tx_artless_enddate',
 		'config' => array(
 			'type'     => 'input',
-			'size'     => '12',
+			'size'     => '13',
 			'max'      => '20',
 			'eval'     => 'datetime',
 			'checkbox' => '0',
 			'default'  => '0'
 		)
 	),
-	'tx_artless_authorpages' => array(
-		'exclude' => 1,
-		'label' => 'LLL:EXT:artless/locallang_db.xml:pages.tx_artless_authorpages',
-		'config' => array(
-			'type' => 'group',
-			'internal_type' => 'db',
-			'allowed' => 'pages',
-			'size' => 3,
-			'minitems' => 0,
-			'maxitems' => 100,
-			'wizards' => array(
-				'suggest' => array(
-					'type' => 'suggest',
-				),
-			),
-		)
-	),
-	'tx_artless_authorfeusers' => array(
-		'exclude' => 0,
-		'label' => 'LLL:EXT:artless/locallang_db.xml:pages.tx_artless_authorfeusers',
+	'tx_artless_place' => array (
+		'exclude' => 1,        
+		'label' => 'LLL:EXT:artless/locallang_db.xml:pages.tx_artless_place',
 		'config' => array (
-			'type' => 'group',
-			'internal_type' => 'db',
-			'allowed' => 'fe_users',
-			'size' => 3,
-			'minitems' => 0,
-			'maxitems' => 100,
-			'wizards' => array(
-				'suggest' => array(
-					'type' => 'suggest',
-				),
-			),
+			'type' => 'input',    
+			'size' => '30',
+        )
+    ),
+	'tx_artless_enablecomments' => array(
+		'exclude' => 1,
+		'label' => 'LLL:EXT:artless/locallang_db.xml:pages.tx_artless_enablecomments',
+		'config' => array(
+			'type' => 'check',
 		)
 	),
 	'tx_artless_categories' => array(
@@ -93,9 +75,9 @@ $tempColumns = array(
 			),
 		)
 	),
-	'tx_artless_relatedpages' => array(
+	'tx_artless_relatives' => array(
 		'exclude' => 1,
-		'label' => 'LLL:EXT:artless/locallang_db.xml:pages.tx_artless_relatedpages',
+		'label' => 'LLL:EXT:artless/locallang_db.xml:pages.tx_artless_relatives',
 		'config' => array(
 			'type' => 'group',
 			'internal_type' => 'db',
@@ -110,128 +92,123 @@ $tempColumns = array(
 			),
 		)
 	),
-	'tx_artless_mediacaptions' => array(
+	'tx_artless_owners' => array(
 		'exclude' => 1,
-		'label' => 'LLL:EXT:artless/locallang_db.xml:pages.tx_artless_mediacaptions',
+		'label' => 'LLL:EXT:artless/locallang_db.xml:pages.tx_artless_owners',
 		'config' => array(
-			'type' => 'text',
-			'cols' => '40',
-			'rows' => '3',
+			'type' => 'group',
+			'internal_type' => 'db',
+			'allowed' => 'pages,fe_users',
+			'size' => 3,
+			'minitems' => 0,
+			'maxitems' => 100,
+			'wizards' => array(
+				'suggest' => array(
+					'type' => 'suggest',
+				),
+			),
 		)
 	),
-	'tx_artless_enablecomments' => array(
-		'exclude' => 1,
-		'label' => 'LLL:EXT:artless/locallang_db.xml:pages.tx_artless_enablecomments',
+	'tx_artless_participants' => array(
+		'exclude' => 0,
+		'label' => 'LLL:EXT:artless/locallang_db.xml:pages.tx_artless_participants',
 		'config' => array(
-			'type' => 'check',
+			'type' => 'group',
+			'internal_type' => 'db',
+			'allowed' => 'pages,fe_users',
+			'size' => 3,
+			'minitems' => 0,
+			'maxitems' => 100,
+			'wizards' => array(
+				'suggest' => array(
+					'type' => 'suggest',
+				),
+			),
 		)
 	),
+	'tx_artless_price' => array(
+		'exclude' => 1,
+		'label' => 'LLL:EXT:artless/locallang_db.xml:pages.tx_artless_price',
+		'config' => array(
+			'type' => 'input',
+			'size' => '13',
+			'eval' => 'trim,double2'
+        )
+    )
 );
 
 t3lib_div::loadTCA('pages');
 t3lib_extMgm::addTCAcolumns('pages', $tempColumns, 1);
-t3lib_extMgm::addToAllTCAtypes("pages","--div--;Artless, tx_artless_startdate,tx_artless_enddate,tx_artless_authorpages,tx_artless_authorfeusers,tx_artless_categories,tx_artless_relatedpages,tx_artless_mediacaptions,tx_artless_enablecomments");
+
+// enable RTE for abstract by copying the configuration of tt_content's bodytext
+$TCA['pages']['columns']['abstract']['config'] = $TCA['tt_content']['columns']['bodytext']['config'];
+
+// set current time as default value of lastUpdated
+$TCA['pages']['columns']['lastUpdated']['config']['default'] = time();
+
+// $TCA['pages']['columns']['media']['config']['size'] = 10;
+// $TCA['pages']['columns']['media']['config']['maxitems'] = 100;
 
 
-$TCA['pages']['columns']['media']['config']['size'] = 10;
-$TCA['pages']['columns']['media']['config']['maxitems'] = 100;
+# var_dump($TCA['pages']['palettes']);
 
-
-/* t3lib_div::array_merge($TCA['pages']['palettes'],array(
-	'1' => array('showitem' => 'starttime, endtime, extendToSubpages'),
-	'2' => array('showitem' => 'layout, lastUpdated, newUntil, no_search'),
-	'3' => array('showitem' => 'alias, target, no_cache, cache_timeout'),
-	'5' => array('showitem' => 'author, author_email', 'canNotCollapse' => 1)
-)); */
-
-/* $TCA['pages']['palettes']['2']['showitem'] = str_replace(
-	'lastUpdated,',
-	'lastUpdated,',
-	$TCA['pages']['palettes']['2']['showitem']
-); */
-
-/*
-$TCA['pages']['palettes']['5'] = array('showitem' => 'author_email, --linebreak--, tx_artless_authorpages, --linebreak--, tx_artless_authorfeusers');
-
-$TCA['pages']['palettes'] = t3lib_div::array_merge($TCA['pages']['palettes'], array(
-	'6' => array('showitem' => 'tx_artless_startdate, tx_artless_enddate', 'canNotCollapse' => 1),
-));
-*/
-
-/*
-$TCA['pages']['types']['1']['showitem'] = str_replace(
-	'--palette--;LLL:EXT:lang/locallang_general.xml:LGL.author;5;;3-3-3,',
-	'crdate,
-	author;;5,',
-	$TCA['pages']['types']['1']['showitem']
+$TCA['pages']['palettes'] = t3lib_div::array_merge(
+	$TCA['pages']['palettes'],
+	array(
+		'time' => array(
+			'showitem' => 'tx_artless_startdate, tx_artless_enddate',
+			'canNotCollapse' => 1
+		),
+		'place' => array(
+			'showitem' => 'tx_artless_place',
+			'canNotCollapse' => 1
+		)
+	)
 );
-*/
 
-/*
-$TCA['pages']['types']['1']['showitem'] = str_replace(
-	'abstract,',
-	'abstract;;;richtext[]:rte_transform[mode=ts],
-	--palette--;LLL:EXT:artless/locallang.xml:A.labels.timePeriod;6;;3-3-3,
-	tx_artless_categories,
-	tx_artless_relatedpages,
-	tx_artless_enablecomments,',
-	$TCA['pages']['types']['1']['showitem']
-);
-*/
 
-/*
+# var_dump($TCA['pages']['types'][t3lib_pageSelect::DOKTYPE_DEFAULT]['showitem']);
+
+# t3lib_extMgm::addToAllTCAtypes('pages', 'crdate', '', 'before:abstract');
+
+// replace the abstract palette with an RTE (2011-02-17: RTE seems not to work within palettes!)
 foreach ($TCA['pages']['types'] as $pageType => &$settings) {
-	
-	if (in_array($pageType, array('3', '4'))) {
-		$settings['showitem'] = str_replace(
-			'--div--;LLL:EXT:cms/locallang_tca.xml:pages.tabs.files,',
-			'--div--;LLL:EXT:cms/locallang_tca.xml:pages.tabs.metadata,
-				crdate,
-				author;;5,
-				abstract;;;richtext[]:rte_transform[mode=ts],
-				--palette--;LLL:EXT:artless/locallang.xml:A.labels.timePeriod;6;;3-3-3,
-				tx_artless_categories,
-				tx_artless_relatedpages,
-				tx_artless_enablecomments,
-				keywords, description,
-			--div--;LLL:EXT:cms/locallang_tca.xml:pages.tabs.files,',
-			$settings['showitem']
-		);
-	}
-	
-	$settings['showitem'] = str_replace(
-		'media,',
-		'media, tx_artless_mediacaptions,',
-		$settings['showitem']
+	// if (in_array($pageType, array('3', '4')))
+	tx_artless_tcatools::modifyShowItemByPaletteKey(
+		$settings['showitem'], 
+		'abstract', 
+		array(
+			'field' => 'abstract',
+			// 'label' => 'LLL:EXT:cms/locallang_tca.xml:pages.palettes.abstract',
+			'palette' => '',
+			'special' => 'richtext:rte_transform[flag=rte_enabled|mode=ts_css]'
+		)
 	);
-	
 }
-*/
 
-
-
-tx_artless_tcatools::modifyShowItemByPaletteKey(
-	$TCA['pages']['types'][t3lib_pageSelect::DOKTYPE_DEFAULT]['showitem'], 
-	'abstract', 
-	array(
-		'field' => 'abstract',
-		//'label' => 'LLL:EXT:cms/locallang_tca.xml:pages.palettes.abstract',
-		'palette' => '',
-		'special' => 'richtext:rte_transform[flag=rte_enabled|mode=ts_css]'
-	)
+t3lib_extMgm::addToAllTCAtypes(
+	'pages',
+	'crdate,--palette--;LLL:EXT:artless/locallang.xml:A.labels.timePeriod;time,--palette--;LLL:EXT:artless/locallang.xml:A.labels.place;place',
+	'1,4,7,3',
+	'before:description'
 );
-
-/*
-tx_artless_tcatools::modifyShowItemByField(
-	&$TCA['pages']['palettes']['abstract']['showitem'], 
-	'abstract', 
-	array(
-		'special' => 'richtext:rte_transform[flag=rte_enabled|mode=ts_css]',
-	)
+t3lib_extMgm::addToAllTCAtypes(
+	'pages',
+	'tx_artless_enablecomments',
+	'1',
+	'before:alias'
 );
-*/
+t3lib_extMgm::addToAllTCAtypes(
+	'pages',
+	'--div--;Relations,tx_artless_categories,tx_artless_relatives,tx_artless_owners,tx_artless_participants'
+);
+t3lib_extMgm::addToAllTCAtypes(
+	'pages',
+	'--div--;Product,tx_artless_price'
+);
 
 
 t3lib_extMgm::addStaticFile($_EXTKEY,'static/artless_typoscript_library/', 'Artless TypoScript Library');
+t3lib_extMgm::addStaticFile($_EXTKEY,'static/colorbox/', 'ColorBox');
 
 ?>
--- ext_tables.sql
+++ ext_tables.sql
@@ -4,14 +4,11 @@
 CREATE TABLE pages (
 	tx_artless_startdate int(11) DEFAULT '0' NOT NULL,
 	tx_artless_enddate int(11) DEFAULT '0' NOT NULL,
-	tx_artless_authorpages text,
-	tx_artless_authorfeusers text,
-	tx_artless_categories text,
-	tx_artless_relatedpages text,
-	tx_artless_mediacaptions text,
+	tx_artless_place text,
 	tx_artless_enablecomments tinyint(3) DEFAULT '0' NOT NULL,
-	# tx_artless_price int(11) DEFAULT '0' NOT NULL,
-	# tx_artless_quantity int(11) DEFAULT '0' NOT NULL,
-	# tx_artless_orders text,
-	# tx_artless_place text,
+	tx_artless_categories text,
+	tx_artless_relatives text,
+	tx_artless_owners text,
+	tx_artless_participants text,
+	price decimal(19,2) DEFAULT '0.00' NOT NULL
 );
